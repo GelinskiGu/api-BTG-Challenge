e0820da84991da02d2f31578bd43d8d8bfa731de
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-07-29 13:37:57 -0300

    First commit

---
+++ src/test/java/com/gelinski/apiBtgChallenge/ApiBtgChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelinski.apiBtgChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBtgChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
